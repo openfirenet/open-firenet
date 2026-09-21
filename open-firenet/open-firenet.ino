79e49c747a4ca648ca23e7605bf66cda3246cb52
openfirenet <openfirenet@lestang.net>
2026-09-21 09:00:10 +0200

    chore(release): bump firmware version to v2.2.1

--- open-firenet/open-firenet.ino
+++ open-firenet/open-firenet.ino
@@ -31,7 +31,7 @@ USBCDC USBSerial;
 
 // --------------------------------------------------------- version & config USB
 #ifndef OPENFIRENET_VERSION
-#define OPENFIRENET_VERSION "2.2.0"
+#define OPENFIRENET_VERSION "2.2.1"
 #endif
 
 // Identifiants USB Open-Firenet
